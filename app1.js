423d55b593e355d8d6fb9d92106ab9470013b107
Dar-Ci Calhoun <darci.calhoun@gmail.com>
2020-10-14 15:38:30 -0700

    added welcome message to app1, added series of questions to app2, deleted app3 (unnecessary to keep)

---
+++ app1.js
@@ -0,0 +1,13 @@
+var userName = prompt('please enter your name')
+
+alert('Hello' + userName);
+
+if(userName == 'Dar-Ci'){
+        document.write('Welcome back, Dar-Ci!')
+    else if(userName == 'William'){
+        document.write('Welcome back, William!')
+    }
+}
+    else {
+        document.write("Welcome," + userName)
+} 
---
+++ app2.js
@@ -0,0 +1,9 @@
+var userBeans1 = prompt('Please enter #1 legume')
+var userBeans2 = prompt('Please enter #2 legume')
+var userBeans3 = prompt('Please enter #3 legume')
+
+document.write(
+    '<li>' + userBeans1 + '</li>' +
+    '<li>' + userBeans2 + '</li>' +
+    '<li>' + userBeans3 + '</li>'
+)
