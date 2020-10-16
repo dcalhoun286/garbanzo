e7a660404e6b35b36310296c42928f71ebc7cd7b
Dar-Ci Calhoun <darci.calhoun@gmail.com>
2020-10-15 23:11:10 -0700

    edited prompts to form complete sentences

--- app2.js
+++ app2.js
@@ -1,13 +1,11 @@
 function favThings(color, number, city){
-    var color = prompt('Please enter your favorite color')
-    var number = prompt('Pick a number 1-10')
-
-    
+    var color = prompt('Please enter your favorite color.')
+    var number = prompt('Please enter your favorite number.')  
 
     while (isNaN(Number(number))){
-        number = prompt('That is not a number. Please enter a number.') 
+        number = prompt('That is not a number. Please enter your favorite number.') 
     }
-    var city = prompt('Please enter your favorite city')
+    var city = prompt('Please enter your favorite city.')
 
     return document.write(
         '<li>' + color + '</li>' +
