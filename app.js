9ec936cf77231d52aa568e56996303c933929ed9
Dar-Ci Calhoun <darci.calhoun@gmail.com>
2020-10-15 18:07:34 -0700

    took code from app1.js and modified it into function and conditional statements in app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+function getName (){
+    var userName = prompt('please enter your name')
+    return 'Hello ' + userName
+}
+
+function greeting(welcome){
+    if (welcome == 'Hello Dar-Ci'){
+        welcome = welcome + '. Welcome back!'
+    }
+ return welcome
+}
