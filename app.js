55570ca0be4d1dfc5cb9d516dccafdc9945ce0eb
Dar-Ci Calhoun <darci.calhoun@gmail.com>
2020-10-15 23:07:25 -0700

    fixed broken code, converted script into functions in app2, added a while loop in app2

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
-function getName (){
+function getName(){
     var userName = prompt('please enter your name')
-    return 'Hello ' + userName
-}
+    return 'Hello, ' + userName
+}    
 
 function greeting(welcome){
-    if (welcome == 'Hello Dar-Ci'){
-        welcome = welcome + '. Welcome back!'
+    if (welcome == 'Hello, Dar-Ci'){
+        welcome += '. Welcome back!'
+    }
+    else {
+        welcome += '.'
     }
  return welcome
+    
 }
--- app2.js
+++ app2.js
@@ -1,9 +1,17 @@
-var userBeans1 = prompt('Please enter your favorite color')
-var userBeans2 = prompt('Please enter your favorite number')
-var userBeans3 = prompt('Please enter your favorite city')
+function favThings(color, number, city){
+    var color = prompt('Please enter your favorite color')
+    var number = prompt('Pick a number 1-10')
 
-document.write(
-    '<li>' + userBeans1 + '</li>' +
-    '<li>' + userBeans2 + '</li>' +
-    '<li>' + userBeans3 + '</li>'
-)
+    
+
+    while (isNaN(Number(number))){
+        number = prompt('That is not a number. Please enter a number.') 
+    }
+    var city = prompt('Please enter your favorite city')
+
+    return document.write(
+        '<li>' + color + '</li>' +
+        '<li>' + number + '</li>' +
+        '<li>' + city + '</li>'
+    )
+}
